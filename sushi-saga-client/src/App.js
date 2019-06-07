c6eff94c1825784c00be6f4192a1c4f27a9ff781
github username <github email address>
2019-06-07 16:00:30 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
-
+import db from './sushis/db.json'
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
-
+  // state = {
+  //   sushis = [];
+  //
+  // }
   render() {
+
+    let s1 = db.sushis[Math.floor(Math.random()*(db.sushis.length))];
+    let s2 = db.sushis[Math.floor(Math.random()*(db.sushis.length))];
+    let s3 = db.sushis[Math.floor(Math.random()*(db.sushis.length))];
+    let s4 = db.sushis[Math.floor(Math.random()*(db.sushis.length))];
+    let sushis = [s1, s2, s3, s4]
+    // console.log(sushis)
     return (
       <div className="app">
-        <SushiContainer  />
+       <SushiContainer sushis={sushis} />
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,15 +1,13 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+  // console.log(props.sushis[0])
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
+      {props.sushis.map(sushi=>{return <Sushi name={sushi.name} id={sushi.id} price={sushi.price} img={sushi.img_url}/>})}
         <MoreButton />
       </div>
     </Fragment>
